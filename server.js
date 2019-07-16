55e9c4c72b359094b967471025df4fa5face46c2
Gant Laborde <gantman@gmail.com>
2019-07-16 13:07:13 -0500

    Netlify (#1)
    Just the worst set of commits ever
    
    
    * adding index
    
    * try to use lambda
    
    * trying to fix redirect
    
    * attempt 2 at netlify
    
    * fix typo
    
    * fixing redirect
    
    * again
    
    * cmon man
    
    * haaaaaands
    
    * no redirect
    
    * random
    
    * fdsaf
    
    * moving to new strategy
    
    * switch to top

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const path = require('path')
-const app = express()
-
-// list of stable snapshots
-const snapshots = ['snap1', 'snap2', 'snap3', 'snap4']
-
-// Make latest build accessible like staging
-app.use('/latest/', express.static(path.join(__dirname, 'build')))
-app.get('/latest/*', (_req, res) =>
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-)
-
-// setup each snapshot build folder
-snapshots.map(snapshotName => {
-  app.use(
-    `/${snapshotName}/`,
-    express.static(path.join(__dirname, snapshotName))
-  )
-  app.get(`/${snapshotName}/*`, (_req, res) =>
-    res.sendFile(path.join(__dirname, snapshotName, 'index.html'))
-  )
-})
-
-// default to latest stable
-app.get('/', (_req, res) => res.redirect(`/${snapshots.slice(-1)}`))
-
-serverless(app)
-app.listen(process.env.PORT || 8080)
-console.log('live')
+const app = require('./express/server');
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Local app listening on port ${port}!`))
