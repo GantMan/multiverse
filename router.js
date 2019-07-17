1ed43bf5eb3fe17f0b0878f8447e5ff7e4d29a6f
Gant Laborde <gantman@gmail.com>
2019-07-17 16:28:20 -0500

    adding mister 2 check

--- router.js
+++ router.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 const router = express.Router()
 
 // list of stable snapshots - move this to JSON file
 const snapshots = ['snap1', 'snap2', 'snap3', 'snap4']
 
+// Depending on import this could be in current path or up a folder
+// this function checks for existence in both paths, returns correct one
+const mister2check = (...checkPath) => {
+  const firstCheck = path.join(__dirname, '../', ...checkPath)
+  if (fs.existsSync(firstCheck)) return firstCheck
+  return path.join(__dirname, ...checkPath)
+}
 
 // setup each snapshot build folder
 snapshots.map(snapshotName => {
   router.use(
     `/${snapshotName}/`,
-    express.static(path.join(__dirname, '../', snapshotName))
+    express.static(mister2check(snapshotName))
   )
   router.get(`/${snapshotName}/*`, (_req, res) =>
-    res.sendFile(path.join(__dirname, '../', snapshotName, 'index.html'))
+    res.sendFile(mister2check(snapshotName, 'index.html'))
   )
 })
 
