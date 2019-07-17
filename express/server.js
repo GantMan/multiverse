07ccccc7de513628b2cd8ff1bee8427a058d3adf
Gant Laborde <gantman@gmail.com>
2019-07-17 15:43:19 -0500

    wtf router created

--- express/server.js
+++ express/server.js
@@ -8,6 +8,12 @@ const newRouter = require('./router')
 // Router so we can use netlify
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(`<script>top.location.href = "/${snapshots.slice(-1)}"</script>`)
+  res.end()
+})
+
 // Setup lambda function
 app.use(bodyParser.json())
 app.use('/.netlify/functions/server', newRouter) // path must route to lambda
